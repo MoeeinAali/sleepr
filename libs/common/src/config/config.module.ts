6015c3cf4c8b4542f3b9d86416254d61c64840c9
Moeein <moeeeinaali@gmail.com>
2026-03-25 12:57:11 +0330

    feat: enhance configuration and database modules with Joi validation

--- libs/common/src/config/config.module.ts
+++ libs/common/src/config/config.module.ts
@@ -1,7 +1,23 @@
 import { Module } from '@nestjs/common';
-import { ConfigModule as NestConfigModule } from '@nestjs/config';
+import {
+  ConfigService,
+  ConfigModule as NestConfigModule,
+} from '@nestjs/config';
+import Joi from 'joi';
 
 @Module({
-  imports: [NestConfigModule.forRoot()],
+  imports: [
+    NestConfigModule.forRoot({
+      validationSchema: Joi.object({
+        NODE_ENV: Joi.string()
+          .valid('development', 'production', 'test')
+          .default('development'),
+        PORT: Joi.number().default(3000),
+        MONGODB_URI: Joi.string().required(),
+      }),
+    }),
+  ],
+  providers: [ConfigService],
+  exports: [ConfigService],
 })
 export class ConfigModule {}
--- libs/common/src/database/database.module.ts
+++ libs/common/src/database/database.module.ts
@@ -1,7 +1,17 @@
 import { Module } from '@nestjs/common';
+import { ConfigService } from '@nestjs/config';
 import { MongooseModule } from '@nestjs/mongoose';
+import { ConfigModule } from '@app/common';
 
 @Module({
-  imports: [MongooseModule.forRoot('mongodb://localhost:27017/sleepr')],
+  imports: [
+    MongooseModule.forRootAsync({
+      imports: [ConfigModule],
+      useFactory: (configService: ConfigService) => ({
+        uri: configService.get('MONGODB_URI'),
+      }),
+      inject: [ConfigService],
+    }),
+  ],
 })
 export class DatabaseModule {}
