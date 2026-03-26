85dda4b9fa031d25fed4235a14ea5f01cf2bb584
Moeein <moeeeinaali@gmail.com>
2026-03-26 17:44:54 +0330

    feat: add Dockerfile and docker-compose configuration for auth service, implement Swagger documentation, and enhance user DTO with validation

--- apps/auth/src/users/dto/create-user.dto.ts
+++ apps/auth/src/users/dto/create-user.dto.ts
@@ -1,9 +1,12 @@
+import { ApiProperty } from '@nestjs/swagger';
 import { IsEmail, IsStrongPassword } from 'class-validator';
 
 export class CreateUserDto {
+  @ApiProperty({ type: String, format: 'email' })
   @IsEmail()
   email: string;
 
+  @ApiProperty({ type: String, format: 'password' })
   @IsStrongPassword()
   password: string;
 }
